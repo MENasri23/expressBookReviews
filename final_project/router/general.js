85015d7bd30be46b1821c5b8f017ae569898e680
MENasri23 <m.e26159487@gmail.com>
2023-11-23 02:49:08 -0800

    Complete task4, task5 and task6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -14,7 +14,7 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  const result = isbns().map(id => books[id]);
+  const result = isbns().map(isbn => books[isbn]);
   res.send(result);
 });
 
@@ -31,20 +31,35 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const bookIsbn = isbns().find(isbn => books[isbn].author === author);
+  if (bookIsbn) {
+    res.send(books[bookIsbn]);
+  } else {
+    return res.status(404).json({message: `Unable to find book for author: ${author}`});
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const bookIsbn = isbns().find(isbn => books[isbn].title === title);
+  if (bookIsbn) {
+    res.send(books[bookIsbn]);
+  } else {
+    return res.status(404).json({message: `Unable to find book for title: ${title}`});
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if (book) {
+    res.send(book.reviews)
+  } else {
+    return res.status(404).json({message: "Unable to find reviews for book with isbn: " + isbn});
+  }
 });
 
 module.exports.general = public_users;
